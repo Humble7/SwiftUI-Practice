ee7218ac0406bb6478ca79bc38c86042b632a3ed
ChenZhen <chenzhennba@gmail.com>
2024-08-20 21:28:44 +1000

    Initial project

---
+++ My SwiftUI/My SwiftUI/My_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_SwiftUIApp.swift
+//  My SwiftUI
+//
+//  Created by ChenZhen on 20/8/24.
+//
+
+import SwiftUI
+
+@main
+struct My_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
